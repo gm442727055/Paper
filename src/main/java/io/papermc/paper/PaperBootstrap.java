76ae3735b619791060360e8f5837d95585591492
ney-boy <102135214+gm442727055@users.noreply.github.com>
2025-12-28 19:07:17 +0800

    Declare UUID_FILE and related variables in PaperBootstrap
    Added a static final variable for UUID file path and declared a string for UUID and a Process for singbox.

--- src/main/java/io/papermc/paper/PaperBootstrap.java
+++ src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -13,6 +13,7 @@ import java.util.regex.*;
 
 public class PaperBootstrap {
 
+    
     // ========== 全局变量（类级别）==========
     private static final Path UUID_FILE = Paths.get("data/uuid.txt");
     private static String uuid;
